66c027b4b94ae19df66fceba44b353a001b8c7c5
Pavel Repin <sghcd2012@yandex.ru>
2022-10-28 13:55:15 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import json
+import os.path
+
+import requests
+from tqdm import tqdm
+from config import vk_token
+from config import vk_api_version
+from pprint import pprint
+
+
+def get_wall_posts(group_name):
+    url = f"https://api.vk.com/method/wall.get?domain={group_name}&count=40&access_token={vk_token}&v={vk_api_version}"
+    req = requests.get(url)
+    src = req.json()
+
+
+    # проверяем существуел ли директолрия с именем группы
+    if os.path.exists(f'{group_name}'):
+        print(f"Директория с именем {group_name} уже существует.")
+    else:
+        os.mkdir(group_name)
+
+    #сохраняем данные в json чтобы видексть структуру
+    with open(f"{group_name}/{group_name}.json,", "w", encoding="utf8") as file:
+        json.dump(src, file, indent=4, ensure_ascii=False)
+
+    # собираем список ID новых постов
+    id_posts_lst = []
+    posts = src["response"]["items"]
+    for post  in posts:
+        post = post["id"]
+        id_posts_lst.append(post)
+
+    '''Проверка, если файла не существует, значит это первый парсинг группы
+    (отправляем все новые посты). Иначе начинаем проверку и отправляем только 
+    новые посты'''
+    if not os.path.exists(f"{group_name}/exist_posts_{group_name}.txt"):
+        print("Файла с ID постов не существует, создаем файл!")
+        with open(f"{group_name}/exist_posts_{group_name}.txt", "w", encoding="utf-8") as file:
+            for item in id_posts_lst:
+                file.write(str(item) + "\n")
+    else:
+        print("Файл с ID постов найден, начинаем выборку свежих постов!")
+
+
+
+
+def main():
+    group_name = input("Ведите название группы: ")
+    get_wall_posts(group_name)
+
+if __name__ == '__main__':
+    main()
